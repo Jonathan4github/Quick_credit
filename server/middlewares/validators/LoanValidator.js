bebd6c59ec1d42ff6779fbb1ca22a128f2e25751
Jonathan Williams <jonathanwilliams505@gmail.com>
2019-05-20 22:31:27 +0100

    feature(approveOrRejecctLoan): Implement approve or reject loan application
    [Finishes #166136932]

--- server/middlewares/validators/LoanValidator.js
+++ server/middlewares/validators/LoanValidator.js
@@ -1,6 +1,6 @@
 import db from '../../models/db';
 
-class LoanValidator {  
+class LoanValidator {
   /**
    * Method for validating loan application
    * @static
@@ -8,17 +8,17 @@ class LoanValidator {
    * @param {response} res
    * @return {obj} return json object.
    */
-  validateLoanApplication(req, res, next){
+  static validateLoanApplication(req, res, next) {
     const { amount, tenor } = req.body;
     let errorMessage = {};
-    
-    if(isNaN(amount) || (amount < 1000) || (amount > 10000000)) {
+
+    if (isNaN(amount) || (amount < 1000) || (amount > 10000000)) {
       errorMessage.amount = 'amount should be number & not less than 1000 or above 10000000';
     }
-    if(isNaN(tenor) || tenor < 1 || tenor > 12){
+    if (isNaN(tenor) || tenor < 1 || tenor > 12) {
       errorMessage.tenor = 'tenor should be number & not less than 1 or above 12';
     }
-    const { status, id} = req.user.rows[0];
+    const { status, id } = req.user.rows[0];
 
     if (status !== 'verified') {
       return res.status(400).send({
@@ -26,16 +26,16 @@ class LoanValidator {
         error: 'You are not yet verified'
       });
     }
-  
+
     const creatQuery = `SELECT * FROM loans WHERE userId = $1`;
-    db.query(creatQuery, [id]).then(loan =>{
-      if (loan.rows !== 0 && loan.rows[0].repaid == false) {        
+    db.query(creatQuery, [id]).then(loan => {
+      if (loan.rows !== 0 && loan.rows[0].repaid == false) {
         return res.status(400).send({
           status: 'Failed',
           error: `You cannot apply. You still have outstanding`
         })
-      } 
-    }).catch(e => (e));    
+      }
+    }).catch(e => (e));
 
     if (!(Object.keys(errorMessage).length === 0)) {
       return res.status(400).json(errorMessage);
@@ -43,36 +43,46 @@ class LoanValidator {
     return next();
   }
 
-  validateQueryParams(req, res, next) {
+  static validateQueryParams(req, res, next) {
     //check if query parimeter is empty  return all loan    
-   let queryParimeters = req.query;
-   if (Object.keys(queryParimeters).length === 0) {
-     return next();
-   }
-   /**
-    * get values from query parimenter
-    */
-   let loanStatus = queryParimeters['status'];
-   let loanRepaid = queryParimeters['repaid'];
-   
-   /**
-    * validate query parimeter if it has 
-    * object property of status and repaid else
-    * throw an error message.
-    */
-   let errorMessage = {};
-   if (!(loanRepaid == 'true' || loanRepaid == 'false')) {
-     errorMessage.repaid = 'repaid value is required & should be true or false'
-   }
-   if (!(loanStatus == 'approved' || loanStatus == 'pending' || loanStatus == 'rejected')) {
-     errorMessage.status = 'status value is required & should be pending, approved, rejected'
-   }
-   if (!(Object.keys(errorMessage).length === 0)) {
-     return res.status(400).json(errorMessage);
-   }
-   return next();
- }
+    let queryParimeters = req.query;
+    if (Object.keys(queryParimeters).length === 0) {
+      return next();
+    }
+    /**
+     * get values from query parimenter
+     */
+    let loanStatus = queryParimeters['status'];
+    let loanRepaid = queryParimeters['repaid'];
 
+    /**
+     * validate query parimeter if it has 
+     * object property of status and repaid else
+     * throw an error message.
+     */
+    let errorMessage = {};
+    if (!(loanRepaid == 'true' || loanRepaid == 'false')) {
+      errorMessage.repaid = 'repaid value is required & should be true or false'
+    }
+    if (!(loanStatus == 'approved' || loanStatus == 'pending' || loanStatus == 'rejected')) {
+      errorMessage.status = 'status value is required & should be pending, approved, rejected'
+    }
+    if (!(Object.keys(errorMessage).length === 0)) {
+      return res.status(400).json(errorMessage);
+    }
+    return next();
+  }
+
+  static loanStatus(req, res, next) {
+    let { status } = req.body;
+    if (!((status == 'approve') || (status == 'reject'))) {
+      return res.status(422).json({
+        status: 'Failed',
+        error: 'status required & should be approve or reject'
+      });
+    }    
+    return next();
+  }
 }
 
-export default new LoanValidator;
+export default LoanValidator;
