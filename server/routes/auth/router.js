2c351ed3e547846d57c17a1cf2dc38a2a215b25b
Jonathan Williams <jonathanwilliams505@gmail.com>
2019-05-22 00:05:12 +0100

    feature(tokenOrPassword): Signin using token or email and password
    [Finishes #166171365]

--- server/routes/auth/router.js
+++ server/routes/auth/router.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import AuthController from '../../controllers/AuthController';
 import AuthValidator from '../../middlewares/validators/AuthValidator';
+import AuthTokenOrPassword from '../../middlewares/AuthTokenOrPassword';
 
 const router = express.Router();
+
+const { verify } = AuthTokenOrPassword;
 
 router.route('/auth/signup/')
   .post(AuthValidator.Signup, AuthController.signup);
 router.route('/auth/signin')
-  .post(AuthValidator.Signin, AuthController.signin);
+  .post(verify, AuthValidator.Signin, AuthController.signin);
 
 export default router;
