615831f25afa69d8286745a93312d18c0d6434db
Jonathan Williams <jonathanwilliams505@gmail.com>
2019-05-24 03:22:16 +0100

    deploy to heroku

--- server/models/seed.js
+++ server/models/seed.js
@@ -11,7 +11,7 @@ const address = 'No 12 ladipo lagos nigeria';
 
 const seedDatabase = (req, res) => {
   const createQuery = 'INSERT INTO users (firstName, lastName, email, password, work_address, home_address, created_date, modified_date, isadmin, status)  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning *';
-  const values = ['admin', 'admin', 'admin@gmail', hashPassword, address, address, moment(new Date()), moment(new Date()), true, 'verified'];
+  const values = ['admin', 'admin', 'admin@gmail.com', hashPassword, address, address, moment(new Date()), moment(new Date()), true, 'verified'];
 
   db.query(createQuery, values).then((user) => {
   }).catch(e=>(e));
